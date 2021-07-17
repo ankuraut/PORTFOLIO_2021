0984823482d7d903c206124289f7af99f5a3c548
Ankit Raut <41611215+ankuraut@users.noreply.github.com>
2021-07-17 20:42:37 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,63 @@
+/*===== menu show ====*/
+const showMenu = (toggleId, navId) =>{
+    const toggle = document.getElementById(toggleId),
+    nav = document.getElementById(navId)
+
+    if(toggle && nav){
+        toggle.addEventListener('click', ()=>{
+            nav.classList.toggle('show')
+        })
+    }
+}
+showMenu('nav-toggle','nav-menu')
+
+/*===== ACTIVE AND REMOVE MENU =====*/
+const navLink = document.querySelectorAll('.nav__link');   
+
+function linkAction(){
+  /*Active link*/
+  navLink.forEach(n => n.classList.remove('active'));
+  this.classList.add('active');
+  
+  /*Remove menu mobile*/
+  const navMenu = document.getElementById('nav-menu')
+  navMenu.classList.remove('show')
+}
+navLink.forEach(n => n.addEventListener('click', linkAction));
+
+/*===== SCROLL REVEAL ANIMATION =====*/
+const sr = ScrollReveal({
+    origin: 'top',
+    distance: '80px',
+    duration: 2000,
+    reset: true
+});
+
+/*SCROLL HOME*/
+sr.reveal('.home__title',{}); 
+sr.reveal('.button',{delay: 200}); 
+sr.reveal('.home__img',{delay: 400}); 
+sr.reveal('.home__social-icon',{ interval: 200}); 
+
+/*SCROLL ABOUT*/
+sr.reveal('.about__img',{}); 
+sr.reveal('.about__subtitle',{delay: 400}); 
+sr.reveal('.about__text',{delay: 400}); 
+
+/*SCROLL SKILLS*/
+sr.reveal('.text-center',{});
+sr.reveal('.text-center-tag-line',{});
+sr.reveal('.flip',{interval: 200}); 
+sr.reveal('.front',{delay: 600});
+
+/*SCROLL PROJECT*/
+sr.reveal('.row', {});
+sr.reveal('.blog_post', {});
+sr.reveal('.blog_post_info', {});
+sr.reveal('.blog_post_img',{interval: 300});
+
+/*SCROLL SEE-MORE*/
+sr.reveal('.see_section_txt', {});
+
+/*SCROLL CONTACT*/
+sr.reveal('.contact__input',{interval: 200}); 
